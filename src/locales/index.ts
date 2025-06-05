3356a086b9926937372d6fe06c3cc48bc7425a45
Tamires Pimenta <tamiresmanhaes4@gmail.com>
2025-06-05 02:18:31 -0300

    feat: initialize project with React, Vite, and Tailwind CSS
    - Added PostCSS configuration for Tailwind CSS.
    - Included Vite logo SVG in the public directory.
    - Created base CSS file with Tailwind and custom styles.
    - Implemented localization support with language codes.
    - Set up main entry point for React application.
    - Defined routing structure with authentication redirection.
    - Added utility functions for class name merging and initials extraction.
    - Configured TypeScript settings for the application.
    - Established Vite configuration with React and Tailwind CSS plugins.

---
+++ src/locales/index.ts
@@ -0,0 +1,14 @@
+export const locales = [
+  {
+    code: "EN",
+    name: "Ingles",
+  },
+  {
+    code: "PT",
+    name: "Portugues",
+  },
+  {
+    code: "ES",
+    name: "Espanha",
+  },
+];
---
+++ src/routes.tsx
@@ -0,0 +1,55 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router";
+import { SignInPage } from "./pages/SignIn";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { SignUpPage } from "./pages/SignUp";
+import { HomePage } from "./pages/Home";
+import { CategoryPage } from "./pages/Category";
+
+function AuthRedirect() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+
+    if (
+      token &&
+      (location.pathname === "/auth/sign-in" ||
+        location.pathname === "/auth/sign-up")
+    ) {
+      navigate("/", { replace: true });
+    }
+
+    if (
+      !token &&
+      location.pathname !== "/auth/sign-in" &&
+      location.pathname !== "/auth/sign-up"
+    ) {
+      navigate("/auth/sign-in", { replace: true });
+    }
+  }, [location, navigate]);
+
+  return null;
+}
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <AuthRedirect />
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/auth/sign-in" element={<SignInPage />} />
+        <Route path="/auth/sign-up" element={<SignUpPage />} />
+        <Route path="/categories/:categoryId" element={<CategoryPage />} />
+        <Route path="/profile" element={<div>Profile</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
