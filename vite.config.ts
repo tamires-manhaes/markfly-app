04a246bf160585e14737ad5490f5efeada7c5ad5
Tamires Pimenta <tamiresmanhaes4@gmail.com>
2025-06-05 15:11:36 -0300

    feat: add vite-plugin-svgr for SVG component support and update tsconfig for type roots

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"), // <-- Add this alias
